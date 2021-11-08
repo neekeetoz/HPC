3932c37cd9d7e083a0a93fe2940fc92cb0562542
neekeetoz <47712811+neekeetoz@users.noreply.github.com>
2021-11-08 17:12:36 +0400

    Delete matmul directory

--- matmul/build.bat
+++
@@ -1,8 +0,0 @@
-del lib\\*.lib
-del cudaext*
-del wrapper.cpp
-rmdir /s /q build
-
-nvcc -I ..\\common -lib -odir lib -o lib/kernel.lib cuda/kernel.cu
-
-python setup.py build_ext -i
--- matmul/cuda/kernel.h
+++
@@ -1,1 +0,0 @@
-void gpu_matmul(float *a, float *b, float *out, int a_dim1, int a_dim2, int b_dim2);
